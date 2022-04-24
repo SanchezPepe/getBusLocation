1f7cbd8e500ccd10e9e807bfcf50104cf3d83dc3
sanchezpepe <sanchezpepe97@gmail.com>
2022-04-24 01:39:18 -0500

    added other locations get methods

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const axios = require('axios');
 const parser = require('xml2json');
 const PORT = 5555;
 
-var xml2jsonOptions = {
-    object: true,
-    coerce: true,
-    sanitize: true,
-    trim: true
-};
-
 let quevedo = "http://bus.itam.mx/servicioubica/servu.asmx/obtenUltimasCoordenadasRuta1Telcel";
 let barranca = "http://bus.itam.mx/servicioubica/servu.asmx/obtenUltimasCoordenadasRuta2Telcel";
 let staTeresa1 = "http://bus.itam.mx/servicioubica/servu.asmx/obtenUltimasCoordenadasRuta3Telcel";
 let staTeresa2 = "http://bus.itam.mx/servicioubica/servu.asmx/obtenUltimasCoordenadasRuta4Telcel";
 
 function getBusLocation(url) {
+    let xml2jsonOptions = {
+        object: true,
+        coerce: true,
+        sanitize: true,
+        trim: true
+    };
+
     return new Promise((resolve, reject) => {
         axios.get(url)
             .then(function(response) {
@@ -32,21 +32,27 @@ function getBusLocation(url) {
 }
 
 app.listen(PORT, () => {
-    console.log(`Server ejecutándose en el puerto ${PORT}`);
+    console.log(`Server running on: ${PORT}`);
 });
 
-app.get('/', async (_, res) => {
+app.get('/quevedo', async (_, res) => {
     let location = await getBusLocation(quevedo);
+    res.json(location);
+});
 
+app.get('/barranca', async (_, res) => {
+    let location = await getBusLocation(barranca);
+    res.json(location);
+});
+
+app.get('/staTeresa1', async (_, res) => {
+    let location = await getBusLocation(staTeresa1);
+    res.json(location);
+});
+
+app.get('/staTeresa2', async (_, res) => {
+    let location = await getBusLocation(staTeresa2);
     res.json(location);
 });
 
 exports.getBus = app;
-
-async function getBusData(url){
-    let busData = await getBusLocation(url);
-    let xml = parser.toJson(busData, xml2jsonOptions)["string"]["$t"];
-    let data = parser.toJson(xml, xml2jsonOptions)
-
-    return data["NewDataSet"]
-}
