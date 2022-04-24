4e354af5ffffe26e9365c952b4ddc04c8575b42c
sanchezpepe <sanchezpepe97@gmail.com>
2022-04-24 01:35:47 -0500

    multiple locations added

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
-const xml2json = require('xml2js').parseString;
+const parser = require('xml2json');
+const PORT = 5555;
 
-let url = "http://bus.itam.mx/servicioubica/servu.asmx/obtenUltimasCoordenadasRuta1Telcel?";
+var xml2jsonOptions = {
+    object: true,
+    coerce: true,
+    sanitize: true,
+    trim: true
+};
 
-function getBusLocation(res, data) {
+let quevedo = "http://bus.itam.mx/servicioubica/servu.asmx/obtenUltimasCoordenadasRuta1Telcel";
+let barranca = "http://bus.itam.mx/servicioubica/servu.asmx/obtenUltimasCoordenadasRuta2Telcel";
+let staTeresa1 = "http://bus.itam.mx/servicioubica/servu.asmx/obtenUltimasCoordenadasRuta3Telcel";
+let staTeresa2 = "http://bus.itam.mx/servicioubica/servu.asmx/obtenUltimasCoordenadasRuta4Telcel";
+
+function getBusLocation(url) {
     return new Promise((resolve, reject) => {
         axios.get(url)
             .then(function(response) {
-                //console.log(response.data);
-                resolve(response.data);
+                let xml = parser.toJson(response.data, xml2jsonOptions)["string"]["$t"];
+                let data = parser.toJson(xml, xml2jsonOptions)
+
+                resolve(data["NewDataSet"]);
             })
             .catch(function(error) {
                 reject(error)
@@ -18,15 +31,22 @@ function getBusLocation(res, data) {
     })
 }
 
-const PORT = 5555;
-
 app.listen(PORT, () => {
     console.log(`Server ejecutándose en el puerto ${PORT}`);
 });
 
-app.get('/', async (req, res) => {
-    let xml = await getBusLocation(url);
-    res.send(data);
+app.get('/', async (_, res) => {
+    let location = await getBusLocation(quevedo);
+
+    res.json(location);
 });
 
 exports.getBus = app;
+
+async function getBusData(url){
+    let busData = await getBusLocation(url);
+    let xml = parser.toJson(busData, xml2jsonOptions)["string"]["$t"];
+    let data = parser.toJson(xml, xml2jsonOptions)
+
+    return data["NewDataSet"]
+}
